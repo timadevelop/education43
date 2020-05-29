0790163207b2f6ebd03dfa0f77681fc39299a78f
timadevelop <timadevelop@gmail.com>
2020-05-29 16:30:45 +0300

    gh actions

---
+++ scripts/release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+log() {
+  printf "\n$1\n"
+}
+
+back_to_main() {
+  log "Going back to main branch..." &&
+  git checkout main &&
+  log "Deleting tmp branch..." &&
+  git branch -D tmp-release
+}
+
+cancel() {
+  log "Some error occurred... cancelling the release process"
+  back_to_main
+}
+
+release() {
+  log "Deleting old branch..."
+  git branch -D tmp-release
+  log "Checking out to a new tmp-release branch..." &&
+  git checkout -b tmp-release &&
+  log "Merging with master..." &&
+  git merge --no-edit master &&
+  log "Unignoring dist folder..." &&
+  sed -i '/dist/d' ./.gitignore &&
+  log "Deleting the index.original.html..." &&
+  echo 'index.original.html' >> ./.gitignore &&
+  log "git add..." &&
+  git add . &&
+  log "commit..." &&
+  git commit -m "Edit .gitignore to release" &&
+  log "Deleting old release-to-gh-pages split branch..." &&
+  (git branch -D release-to-gh-pages &>/dev/null || true) &&
+  log "Subtree split ..." &&
+  git subtree split --prefix dist/education/browser/ -b release-to-gh-pages &&
+  log "Pushing to gh-pages..." &&
+  git push origin release-to-gh-pages:gh-pages --force
+}
+
+if [ -z "$(git status --porcelain)" ]
+then
+  (release && back_to_main) || cancel
+else
+  log "Clean working directory to release"
+fi
+
